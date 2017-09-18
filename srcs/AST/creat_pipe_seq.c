8cf0701d7267549e9cc0999db09fcee7e2239bed
Nicolas BOUCHINET <nbouchin@e1r11p21.42.fr>
2017-09-18 09:19:47 +0200

    normed few files

--- srcs/AST/creat_pipe_seq.c
+++ srcs/AST/creat_pipe_seq.c
@@ -6,7 +6,7 @@
 /*   By: zadrien <zadrien@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/29 10:44:26 by zadrien           #+#    #+#             */
-/*   Updated: 2017/09/17 18:12:12 by zadrien          ###   ########.fr       */
+/*   Updated: 2017/09/18 08:29:08 by nbouchin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,26 +52,16 @@ void			simple_sequence(t_ast **ast, t_tok **lst, t_tok **sep)
 
 	tmp = *lst;
 	tmp_ast = *ast;
-	// while (tmp && tmp != *sep && tmp->str == LOCAL)
-	// {
-	// 	export(tmp->str, NULL, NULL, EXPORT);
-	// 	tmp = tmp->next;
-	// }
-	// if (tmp != *sep)
-	// {
-		init_ast(&tmp_ast->left, &tmp,
+	init_ast(&tmp_ast->left, &tmp,
 		ft_strrchr(tmp->str, '/') ? CMD_NAME_ABS : CMD_NAME_RLT);
+	tmp = tmp->n;
+	while (tmp != *sep)
+	{
+		init_ast(&tmp_ast->right, &tmp, CMD_ARG);
+		if (tmp->n != *sep)
+			tmp_ast = tmp_ast->right;
 		tmp = tmp->n;
-		while (tmp != *sep)
-		{
-			init_ast(&tmp_ast->right, &tmp, CMD_ARG);
-			if (tmp->n != *sep)
-				tmp_ast = tmp_ast->right;
-			tmp = tmp->n;
-		}
-	// 	return (1);
-	// }
-	// return (-1);
+	}
 }
 
 void			command_sequence(t_ast **ast, t_tok **lst, t_tok **sep)
--- srcs/builtins/history_no_opt.c
+++ srcs/builtins/history_no_opt.c
@@ -6,35 +6,35 @@
 /*   By: khabbar <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/18 16:35:31 by khabbar           #+#    #+#             */
-/*   Updated: 2017/09/12 08:51:44 by nbouchin         ###   ########.fr       */
+/*   Updated: 2017/09/18 08:30:16 by nbouchin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
 
-void    no_options(t_his **his, int offset, int len, int i)
+void	no_options(t_his **his, int offset, int len, int i)
 {
-  int     nbr;
+	int	nbr;
 
-  nbr = 0;
-  if (i == 7 && (!offset || offset > len))
+	nbr = 0;
+	if (i == 7 && (!offset || offset > len))
     while ((*his) && ft_strcmp((*his)->cmdl, ""))
     {
       ft_printf("    %d  %s\n", ++nbr, (*his)->cmdl);
       (*his) = (*his)->n;
     }
-  else if (i == 7 && offset)
-  {
-    len = len - offset;
-    while (--len)
-    {
-      (*his) = (*his)->n;
-      nbr += 1;
-    }
-    while ((*his) && ft_strcmp((*his)->cmdl, ""))
-    {
-      ft_printf("    %d  %s\n", ++nbr, (*his)->cmdl);
-      (*his) = (*his)->n;
-    }
-  }
+	else if (i == 7 && offset)
+	{
+		len = len - offset;
+		while (--len)
+		{
+			(*his) = (*his)->n;
+			nbr += 1;
+		}
+		while ((*his) && ft_strcmp((*his)->cmdl, ""))
+		{
+			ft_printf("    %d  %s\n", ++nbr, (*his)->cmdl);
+			(*his) = (*his)->n;
+		}
+	}
 }
